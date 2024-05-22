a584ff28c92bcad3601e969c5908875a76bfd2ae
stenlyysamberi <samstenly815@gmail.com>
2024-05-23 01:00:14 +0800

    first commit

--- assets/js/costum.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".humberger").click(function(){
-        $(".wrapper-nav").toggleClass("show");
-        $(".icon-bar").toggleClass("d-none");
-        $(".close").toggleClass("d-none")
-
-    });
-});
